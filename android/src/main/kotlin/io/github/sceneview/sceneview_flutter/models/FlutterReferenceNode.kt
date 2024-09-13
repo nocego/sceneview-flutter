ef983f6b1a783c1cfa6c6fbeeac3de4a596e711b
Gerold Abgottspon <gerold_abg@hotmail.com>
2024-09-13 16:19:23 +0200

    revert changes

--- android/src/main/kotlin/io/github/sceneview/sceneview_flutter/models/FlutterReferenceNode.kt
+++ android/src/main/kotlin/io/github/sceneview/sceneview_flutter/models/FlutterReferenceNode.kt
@@ -9,20 +9,17 @@ data class FlutterReferenceNode(
     val id: String,
     override val position: FloatArray,
     override val rotation: FloatArray,
-    val fileLocation: String,
-    val scale: FloatArray // Add scale property
+    val fileLocation: String
 ) : FlutterSceneViewNode() {
 
     fun getPositionFloat3(): Float3 = Float3(position[0], position[1], position[2])
     fun getRotationFloat3(): Float3 = Float3(rotation[0], rotation[1], rotation[2])
-    fun getScaleFloat3(): Float3 = Float3(scale[0], scale[1], scale[2]) // Add scale conversion
 
     companion object {
         fun from(map: Map<String, *>): FlutterReferenceNode {
             Log.d("FlutterReferenceNode", "Creating node from map: $map")
             val positionMap = map["position"] as Map<String, Double>
             val rotationMap = map["rotation"] as Map<String, Double>
-            val scaleMap = map["scale"] as Map<String, Double> // Add scale map
             val node = FlutterReferenceNode(
                 id = map["id"] as String,
                 position = floatArrayOf(
@@ -36,12 +33,7 @@ data class FlutterReferenceNode(
                     rotationMap["z"]?.toFloat() ?: 0f,
                     rotationMap["w"]?.toFloat() ?: 1f
                 ),
-                fileLocation = map["fileLocation"] as String,
-                scale = floatArrayOf( // Add scale initialization
-                    scaleMap["x"]?.toFloat() ?: 0.5f,
-                    scaleMap["y"]?.toFloat() ?: 0.5f,
-                    scaleMap["z"]?.toFloat() ?: 0.5f
-                )
+                fileLocation = map["fileLocation"] as String
             )
             Log.d("FlutterReferenceNode", "Created node: $node")
             return node
